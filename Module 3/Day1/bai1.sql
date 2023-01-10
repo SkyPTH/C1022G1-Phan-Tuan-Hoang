bc26245f8be1435ee632423bde8d4637b43e4928
SkyPTH <tuanhoang96dn@gmail.com>
2023-01-10 17:00:10 +0700

    m

--- Module 3/Day1/bai1.sql
+++ Module 3/Day1/bai1.sql
@@ -1,10 +1,17 @@
+create database module_3;
+use module_3;
 create table class ( 
 id int,
-`name` varchar(50)
+`name` varchar(50),
+primary key (id)
 );
 create table teacher (
 id int,
 `name` varchar(50),
 age int,
-`country` varchar (30) 
+`country` varchar (30),
+primary key (id)
 );
+insert into teacher (id,`name`,age,`country`) values (1,"Thảo",18,"Việt Nam");
+select * from teacher;
+select id "Id", age "Tuổi", `country` "Nước" from teacher;
