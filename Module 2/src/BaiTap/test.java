afb1337a9a5be3bec75cafb5dcd34dfbae67c01b
SkyPTH <tuanhoang96dn@gmail.com>
2023-02-26 02:36:43 +0700

    update

--- Module 2/src/BaiTap/test.java
+++ Module 2/src/BaiTap/test.java
@@ -4,26 +4,7 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        String str = "abbbdcef";
-        String str2 = "";
-        int count = 1;
-        for (int i = 0; i < str.length(); ) {
-            for (int j = 1; j < str.length(); j++) {
-                if (str.charAt(i) == str.charAt(j)) {
-                    str = str.substring(0, j) + str.substring(j + 1);
-                    count++;
-                }
-            }
-            if (count > 1) {
-                str2 += str.charAt(i);
-            }
-            count = 1;
-            str = str.substring(1);
-            if (str.length() <= 1) {
-                break;
-            }
-        }
-        String []arr=str2.split("");
-        System.out.println(Arrays.toString(arr));
+        int x=3922;
+        System.out.println(x%2);
     }
 }
---
+++ Module 3/Case database/database_furama.sql
@@ -0,0 +1,111 @@
+CREATE SCHEMA casestudy_furama;
+USE casestudy_furama;
+CREATE TABLE vi_tri (
+    ma_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vi_tri VARCHAR(50)
+);
+CREATE TABLE trinh_do (
+    ma_trinh_do INT PRIMARY KEY AUTO_INCREMENT,
+    ten_trinh_do VARCHAR(50)
+);
+CREATE TABLE bo_phan (
+    ma_bo_phan INT PRIMARY KEY AUTO_INCREMENT,
+    ten_bo_phan VARCHAR(50)
+);
+CREATE TABLE nhan_vien (
+    ma_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
+    ho_ten VARCHAR(50) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(50) NOT NULL,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    dia_chi VARCHAR(50),
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    CONSTRAINT fk_nhan_vien_ma_vi_tri FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri (ma_vi_tri),
+    CONSTRAINT fk_nhan_vien_ma_trinh_do FOREIGN KEY (ma_trinh_do)
+        REFERENCES trinh_do (ma_trinh_do),
+    CONSTRAINT fk_nhan_vien_ma_bo_phan FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
+);
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_khach VARCHAR(50)
+);
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
+    ma_loai_khach INT NOT NULL,
+    ho_ten VARCHAR(50) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(50) NOT NULL,
+    so_dien_thoai VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    dia_chi VARCHAR(50),
+    CONSTRAINT fk_khach_hang_ma_loai_khach FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach)
+);
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_dich_vu VARCHAR(50)
+);
+
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY AUTO_INCREMENT,
+    ten_kieu_thue VARCHAR(50)
+);
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dich_vu VARCHAR(50) NOT NULL,
+    dien_tich INT NOT NULL,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_da INT NOT NULL,
+    ma_kieu_thue INT NOT NULL,
+    ma_loai_dich_vu INT NOT NULL,
+    tieu_chuan_phong VARCHAR(50),
+    mo_ta_tien_nghi_khac VARCHAR(50),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phi_di_kem TEXT,
+    CONSTRAINT fk_dich_vu_ma_loai_dich_vu FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    CONSTRAINT fk_dich_vu_ma_kieu_thue FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue)
+);
+
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY AUTO_INCREMENT,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT NOT NULL,
+    ma_khach_hang INT NOT NULL,
+    ma_dich_vu INT NOT NULL,
+    CONSTRAINT fk_hop_dong_ma_nhan_vien FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien),
+    CONSTRAINT fk_hop_dong_ma_khach_hang FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang),
+    CONSTRAINT fk_hop_dong_ma_dich_vu FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
+);
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dich_vu_di_kem VARCHAR(50) NOT NULL,
+    gia DOUBLE NOT NULL,
+    don_vi VARCHAR(50) NOT NULL,
+    trang_thai VARCHAR(50)
+);
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY AUTO_INCREMENT,
+    ma_hop_dong INT NOT NULL,
+    ma_dich_vu_di_kem INT NOT NULL,
+    so_luong INT NOT NULL,
+    CONSTRAINT fk_hdct_ma_hop_dong FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+    CONSTRAINT fk_hdct_ma_dich_vu_di_kem FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem)
+);
+-- DROP SCHEMA casestudy_furama;
